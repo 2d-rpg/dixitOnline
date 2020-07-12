751cbadc219c423204cb68e579f91fb9006ec6b2
yoshikawajunpei <yoshikawa.j.ac@m.titech.ac.jp>
2020-07-12 21:05:30 +0900

    masterHandSelectionを書いて退避

--- src/client.js
+++ src/client.js
@@ -1,15 +1,12 @@
 'use strict';
 // クライアント側
-// import modules
-import {Init} from './modules/stage/client/init.js'
-import {Waiting} from './modules/stage/client/waiting.js'
-import {Utils} from './modules/stage/client/utils.js'
-// const init = require('./modules/stage/client/init');
 // Socket.IOを利用してサーバに接続
 const socket = io();
 const canvas = document.getElementById('canvas-2d');
 const context = canvas.getContext('2d');
 const startButton = document.getElementById('startButton');
+
+let lst_cards = [];
 
 // サーバーから'state'がemitされたときの動作
 socket.on('state', (players) => {
@@ -43,23 +40,26 @@ function updateNumOfPepole(num) {
  ****************************/
 
 // 接続完了('connect')時の動作(最初の接続完了時に'connect'がサーバーからemitされる)
-socket.on('connect', function(socket){Init.do(socket);});
+socket.on('connect', init);
 // サーバーから'cannot_play'がemitされた時の動作
 socket.on('cannot_play', cannotPlay);
 // サーバーから'waiting'がemitされた時の動作
-socket.on('waiting', Waiting.do);
+socket.on('waiting', waiting)
 // サーバーから'master_hand_selection'がemitされた時の動作
 socket.on('master_hand_selection', function(data){masterHandSelection(data);});
 
-socket.on('master_hand_selection',masterHandSelection)
-
+// init画面
+function init(){
+    socket.emit('init');
+    console.log('[debug] init状態');
+}
 
 // エントリーフォームにsubmitされたときの動作
 $("#entryForm").submit(function(e){
     var username = $("#userName").val();
     // 名前入力フォーム非表示
     document.getElementById("entryForm").style.display = 'none';
-    Utils.clearDisplay();
+    clearDisplay();
     const fontSize = 20;
     context.font = fontSize + 'px Bold Arial';
     const message = '参加者が集まるまでしばらくお待ちください';
@@ -73,7 +73,7 @@ $("#entryForm").submit(function(e){
 function cannotPlay() {
     // 名前入力フォーム表示
     document.getElementById("entryForm").style.display = 'block';
-    Utils.clearDisplay();
+    clearDisplay();
     const fontSize = 20;
     context.font = fontSize + 'px Bold Arial';
     const message = '現在プレイ中です しばらくお待ちください';
@@ -81,10 +81,17 @@ function cannotPlay() {
     context.fillText(message, canvas.width / 2 - message.length * fontSize / 2, canvas.height / 2 - fontSize / 2);
 }
 
+// waiting画面
+function waiting() {
+    clearDisplay();
+    startButton.style.display = 'block';
+    console.log('[debug] waiting状態');
+}
+
 // スタートボタンを押した時の動作
 startButton.onclick = function() {
     startButton.style.display = 'none';
-    Utils.clearDisplay();
+    clearDisplay();
     const fontSize = 20;
     context.font = fontSize + 'px Bold Arial';
     const message = '他の参加者がスタートするのを待っています...';
@@ -93,9 +100,43 @@ startButton.onclick = function() {
     socket.emit('entry_done');
 }
 
+var x = 0;
+var y = 0;
+function Click1(e) {
+    /*
+     * rectでcanvasの絶対座標位置を取得し、
+     * クリック座標であるe.clientX,e.clientYからその分を引く
+     * ※クリック座標はdocumentからの位置を返すため
+     * ※rectはスクロール量によって値が変わるので、onClick()内でつど定義
+     */
+    var rect = e.target.getBoundingClientRect();
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    draw1();
+    let len = lst_cards.length;
+    let matrix_card = -1;
+    for(let i=0;i < len;i++){
+        if(lst_cards[i][0] <= x && x <= lst_cards[i][1] && lst_cards[i][2] <= y && y <= lst_cards[i][3]){
+            matrix_card = i;
+            break;
+        }
+    }
+    if(matrix_card != -1){
+        socket.emit('master_card_select', {matrix_card : matrix_card});
+    }
+}
+
+function draw1() {
+    // 描画処理
+    context.fillRect(x, y, 10, 10);
+}
+
+canvas.addEventListener('click', Click1, false);
+
+
 // master_hand_selection画面
 function masterHandSelection(data) {
-    Utils.clearDisplay();
+    clearDisplay();
     // TODO: srcを変更しているのに，drawImageでの表示が変更されない
     // TODO: 応急処置としてhtmlにサーバーにある画像を追加し，そこから持ってくることにしているがダサい
     const y = 600;
@@ -103,7 +144,18 @@ function masterHandSelection(data) {
         var x = 400.0 / 3.0 + index * (100 + 400.0 / (3.0 * 5.0));
         var image = document.getElementById(card.filename);
         context.drawImage(image, 0, 0, image.width, image.height, x, y, 100, 150);
+        if(player.isMaster){
+            lst_cards.push([x-image.width/2,x+image.width/2,y-image.height/2,y+image.height/2]);
+        }
     });
+    if(player.isMaster == true){
+        const message = 'あなたは語り部です。カードを選択してください';
+    }else{
+        const message = 'あなたは聞き手です。待機中です。'
+    }
+    context.fillText(message, canvas.width / 2 - message.length * fontSize / 2, canvas.height / 2 - fontSize / 2);
+    context.Text
+
     // プレイヤー名/スコアの表示
     // 自分
     const fontSize = 20;
@@ -117,6 +169,18 @@ function masterHandSelection(data) {
         context.fillText(player.name, index * 400 + 200, 200);
         context.fillText('スコア: ' + player.score, index * 400 + 200, 200 + 40);
     });
+}
+
+// ゲーム画面クリア
+function clearDisplay() {
+    // 指定範囲をクリア
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // 枠線の太さ
+    context.lineWidth = 10;
+    // 現在のパスをリセット
+    context.beginPath();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.stroke();
 }
 
 // チャットフォームにsubmitされた時の動作
