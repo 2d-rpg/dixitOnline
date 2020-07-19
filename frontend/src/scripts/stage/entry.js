082052138334f05d818f9ef742c58563e6ba6cb3
nyatadecocoa <yoshii.y.ad@m.titech.ac.jp>
2020-07-19 14:54:32 +0900

    ボタン等の表示~hand_selection手前まで

--- frontend/src/scripts/stage/entry.js
+++ frontend/src/scripts/stage/entry.js
@@ -3,15 +3,17 @@ import { useForm } from 'react-hook-form';
 
 export default function Entry(props) {
     const { register, handleSubmit } = useForm();
+    const [show, setShow] = useState(true);
 
     const onSubmit = (data, event) => {
         // サーバーに'entry'を送信
+        setShow(false);
         props.socket.emit('entry', {username : data.username});
         event.preventDefault(); // フォームによる/?への接続を止める(socketIDを一意に保つため)
     }
 
     return (
-        <form className="form-inline" id="entryForm" onSubmit={ handleSubmit(onSubmit) }>
+        <form className="form-inline" id="entryForm" onSubmit={ handleSubmit(onSubmit) } style={ {display: show ? 'block' : 'none' } }>
             <label className="sr-only" htmlFor="inlineFormInputName2">Name</label>
             <input type="text" className="form-control mb-2 mr-sm-2" id="userName" name="username" ref={ register } placeholder="名前を入力してください"/>
             <button type="submit" className="btn btn-primary mb-2">さんとしてゲームに参加</button>
