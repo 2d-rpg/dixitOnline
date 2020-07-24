0e378095e308a44c40cd3bb5cb274a304a1a9a0d
nyatadecocoa <yoshii.y.ad@m.titech.ac.jp>
2020-07-24 10:43:09 +0900

    filed_selection選択可

--- backend/src/scripts/stage/field_selection.js
+++ backend/src/scripts/stage/field_selection.js
@@ -1,12 +1,14 @@
 
+const utils = require('../utils');
 
 class FieldSelection {
 
     static do(socket, index, game) {
         // 答えを集計
-        id = socket.id;
-        answer = index;
-        dict = {}
+        utils.logWithStage(game.stage, 'socket id: [' + socket.id + ']\'s Player was selected.');
+        let id = socket.id;
+        let answer = index;
+        let dict = {}
         dict['id'] = id;
         dict['answer'] = answer;
         game.answers.push(dict);
